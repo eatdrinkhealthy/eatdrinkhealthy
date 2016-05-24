873f71520cf3e460b36e53af2f504a48b7f2af37
Karl Danninger <kdanninger@gmail.com>
2016-05-24 11:55:25 -0400

    add a TODO for when we want facebook to review our profile process

---
+++ client/main.js
@@ -0,0 +1,8 @@
+Accounts.ui.config({
+  requestPermissions: {
+    facebook: [
+      'public_profile',
+      // 'user_location' TODO: This will trigger a manual review process by facebook! complete user profile before collecting this
+    ]
+  }
+});
