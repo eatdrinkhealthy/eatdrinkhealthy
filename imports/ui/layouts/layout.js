f4b759e1735ee3a9adcdd8205898d75ecd424a72
Karl Danninger <kdanninger@gmail.com>
2016-05-26 16:07:32 -0400

    give the statusbar a little color on iOS

--- imports/ui/layouts/layout.js
+++ imports/ui/layouts/layout.js
@@ -6,11 +6,8 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 // import { FlowRouter } from "meteor/kadira:flow-router";
 
-
 Meteor.startup(() => {
-
 });
 
 Template.layout.helpers({
-
 });
