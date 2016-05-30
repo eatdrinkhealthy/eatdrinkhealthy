466ea39b902f636b3c21f69e676a8aac796f3830
Karl Danninger <kdanninger@gmail.com>
2016-05-30 10:25:37 -0400

    a fix to fixtures :P

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -3,33 +3,35 @@ import { Lists } from "../../api/lists/lists.js";
 
 // Insert some list items if there is nothing in the db on startup.
 Meteor.startup(() => {
-  if (Lists.find().count() === 0 && Meteor.users.find().count() > 0) {
-    const newLists = [
-      {
-        title: "Vegan Shops",
-        description: "A list of my favourite local vegan shops.",
-        venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
-      },
-      {
-        title: "Vegetarian Shops",
-        description: "A list of my favourite local vegetarian shops.",
-        venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
-      },
-      {
-        title: "Gluten-Free Restaurants",
-        description: "A list of my favourite local Gluten-Free restaurants.",
-        venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
-      }
-    ];
+  const newLists = [
+    {
+      title: "Vegan Shops",
+      description: "A list of my favourite local vegan shops.",
+      venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
+    },
+    {
+      title: "Vegetarian Shops",
+      description: "A list of my favourite local vegetarian shops.",
+      venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
+    },
+    {
+      title: "Gluten-Free Restaurants",
+      description: "A list of my favourite local Gluten-Free restaurants.",
+      venues: ["8732gyruu3a", "3da8gyui3", "a398yhiuda"],
+    }
+  ];
 
-    newLists.forEach((list) => {
-      Lists.insert({
-        title: list.title,
-        description: list.description,
-        venues: list.venues,
-        author: Meteor.users.findOne()._id,
-        dateCreated: new Date()
+  Meteor.users.find().fetch().forEach((user) => {
+    if (Lists.find({ author: user._id }).count() === 0) {
+      newLists.forEach((list) => {
+        Lists.insert({
+          title: list.title,
+          description: list.description,
+          venues: list.venues,
+          author: user._id,
+          dateCreated: new Date()
+        });
       });
-    });
-  }
+    }
+  });
 });
