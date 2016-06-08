68481984d205301dad7a05cf83d4fb2e175296af
Karl Danninger <kdanninger@gmail.com>
2016-06-08 12:34:17 -0400

    add icons

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 /* global App */
 App.info({
   id: "co.EatDrinkHealthy.EatDrinkHealthy",
-  name: "eatdrinkhealthy",
+  name: "Eat Drink Healthy",
   description: "TODO",
   author: "Steve Marsh",
   email: "sjm.atl@gmail.com",
@@ -17,11 +17,13 @@ App.icons({
   iphone_3x: "public/icons/icon-180x180.png",
   ipad: "public/icons/icon-76x76.png",
   ipad_2x: "public/icons/icon-152x152.png",
-  ipad_pro: "public/icons/icon-167x167",
+  ipad_pro: "public/icons/icon-167x167.png",
   ios_spotlight: "public/icons/icon-40x40.png",
   ios_spotlight_2x: "public/icons/icon-80x80.png",
+  ios_spotlight_3x: "public/icons/icon-120x120.png",
   ios_settings: "public/icons/icon-29x29.png",
   ios_settings_2x: "public/icons/icon-58x58.png",
+  ios_settings_3x: "public/icons/icon-87x87.png",
 
   // Android
   // android_ldpi: "public/icons/icon-36.png",
@@ -32,13 +34,13 @@ App.icons({
 
 App.launchScreens({
   // iOS
-  iphone: "public/splash/splash-320x480.png",
-  iphone_2x: "public/splash/splash-640x960.png",
-  iphone5: "public/splash/splash-640x1136.png",
-  iphone6: "public/splash/splash-750x1334.png",
-  iphone6p_portrait: "public/splash/splash-1242x2208.png",
-  ipad_portrait: "public/splash/splash-768x1024.png",
-  ipad_portrait_2x: "public/splash/splash-1536x2048.png",
+  // iphone: "public/splash/splash-320x480.png",
+  // iphone_2x: "public/splash/splash-640x960.png",
+  // iphone5: "public/splash/splash-640x1136.png",
+  // iphone6: "public/splash/splash-750x1334.png",
+  // iphone6p_portrait: "public/splash/splash-1242x2208.png",
+  // ipad_portrait: "public/splash/splash-768x1024.png",
+  // ipad_portrait_2x: "public/splash/splash-1536x2048.png",
 
   // Android
   // android_ldpi_portrait: "public/splash/splash-320x426.png",
