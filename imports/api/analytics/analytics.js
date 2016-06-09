ca8ccd1ca583d7ff468265d4d0d02c5b8abae7e5
Karl Danninger <kdanninger@gmail.com>
2016-06-09 17:52:00 -0400

    add more analytics

--- imports/api/analytics/analytics.js
+++ imports/api/analytics/analytics.js
@@ -2,7 +2,9 @@ import { $ } from "meteor/jquery";
 /* global analytics */
 
 export function registerAnalyticsHandlers() {
+  console.log("Registering...");
   $("[data-track]").each((i, block) => {
+    console.log("found: ", $(block).data("track"));
     $(block).click(() => {
       analytics.track(`click: ${$(block).data("track")}`);
     });
