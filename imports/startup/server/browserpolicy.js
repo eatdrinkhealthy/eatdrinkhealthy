04e0d1bddc1dd5fe36ec8eecefb3ba4d1b5f8e79
Karl Danninger <kdanninger@gmail.com>
2016-06-27 17:55:02 -0400

    transpose things we care about out of miraclegrow into edh

--- imports/startup/server/browserpolicy.js
+++ imports/startup/server/browserpolicy.js
@@ -1,5 +1,13 @@
 import { BrowserPolicy } from "meteor/browser-policy-common";
 
+// fonts
+BrowserPolicy.content.allowOriginForAll("fonts.googleapis.com");
+BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
+
+// analytics
+BrowserPolicy.content.allowOriginForAll("www.google-analytics.com");
+BrowserPolicy.content.allowOriginForAll("cdn.mxpnl.com");
+
 BrowserPolicy.content.allowOriginForAll("https://maps.googleapis.com");
 BrowserPolicy.content.allowOriginForAll("https://scontent.xx.fbcdn.net");
 BrowserPolicy.content.allowOriginForAll("https://graph.facebook.com");
--- imports/startup/server/seeds.js
+++ imports/startup/server/seeds.js
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 import { ServiceConfiguration } from "meteor/service-configuration";
 import { Lists } from "../../api/lists/lists.js";
+import { Roles } from "meteor/alanning:roles";
 
 // Insert some list items if there is nothing in the db on startup.
 if (Meteor.isServer) {
@@ -12,7 +14,7 @@ if (Meteor.isServer) {
       venues: [
         "4b6c4f7cf964a5209f2f2ce3",
         "4c0d39fec700c9b6a116a2dd",
-        "54fd7b9d498ed10df2a3c248"
+        "54fd7b9d498ed10df2a3c248",
       ],
     },
     {
@@ -21,7 +23,7 @@ if (Meteor.isServer) {
       venues: [
         "4b6c4f7cf964a5209f2f2ce3",
         "4c0d39fec700c9b6a116a2dd",
-        "54fd7b9d498ed10df2a3c248"
+        "54fd7b9d498ed10df2a3c248",
       ],
     },
     {
@@ -30,12 +32,105 @@ if (Meteor.isServer) {
       venues: [
         "4b6c4f7cf964a5209f2f2ce3",
         "4c0d39fec700c9b6a116a2dd",
-        "54fd7b9d498ed10df2a3c248"
+        "54fd7b9d498ed10df2a3c248",
       ],
     },
   ];
 
+  const addUser = (userInfo) => {
+    let retVal;
+    if (Accounts.createUser) {
+      retVal = Accounts.createUser(userInfo);
+    } else {
+      // accounts-password is not added
+      const userId = Meteor.users.insert(userInfo);
+      retVal = Meteor.users.findOne(userId);
+    }
+    return retVal;
+  };
   Meteor.startup(() => {
+    // Some seed users for every app. Each app can modify these or add other
+    // seed data as desired.
+    // This only runs if accounts-password is added (Accounts.createUser is
+    // defined in that package)
+
+    if (Meteor.users.find().count() === 0) {
+      console.log("Inserting seed users ..."); // eslint-disable-line no-console
+
+      addUser({
+        username: "turing",
+        email: "turing@example.com",
+        password: "password",
+        profile: {
+          name: "Alan Turing",
+        },
+      });
+
+      addUser({
+        username: "knuth",
+        email: "knuth@example.com",
+        password: "password",
+        profile: {
+          name: "Ada Lovelace",
+        },
+      });
+
+      addUser({
+        username: "lovelace",
+        email: "lovelace@example.com",
+        password: "password",
+        profile: {
+          name: "Ada Lovelace",
+        },
+      });
+
+      addUser({
+        username: "kernighan",
+        email: "kernighan@example.com",
+        password: "password",
+        profile: {
+          name: "Brian Kernighan",
+        },
+      });
+
+      addUser({
+        username: "ritchie",
+        email: "ritchie@example.com",
+        password: "password",
+        profile: {
+          name: "Dennis Ritchie",
+        },
+      });
+
+      addUser({
+        username: "dijkstra",
+        email: "dijkstra@example.com",
+        password: "password",
+        profile: {
+          name: "Edsger Dijkstra",
+        },
+      });
+
+      addUser({
+        username: "wozniak",
+        email: "wozniak@example.com",
+        password: "password",
+        profile: {
+          name: "Steve Wozniak",
+        },
+      });
+
+      const admin = addUser({
+        username: "admin",
+        email: "admin@example.com",
+        password: "password",
+        profile: {
+          name: "Admin",
+        },
+      });
+      Roles.addUsersToRoles(admin, ["admin"]);
+    }
+
     if (Lists.find().count() === 0) {
       console.log("Inserting seed data..."); // eslint-disable-line no-console
 
@@ -85,3 +180,4 @@ if (Meteor.isServer) {
     }
   };
 }
+
