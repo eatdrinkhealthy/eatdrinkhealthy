6e71993f18627af99bedfc53054b7d60512262d6
stevenjmarsh <sjm.atl@gmail.com>
2017-06-29 14:56:54 -0400

    fixing Facebook oAuth login (also, appId & secret read from settings)

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,4 +1,17 @@
+import { ServiceConfiguration } from "meteor/service-configuration";
 import { Accounts } from "meteor/accounts-base";
+import { Meteor } from "meteor/meteor";
+
+ServiceConfiguration.configurations.upsert(
+  { service: "facebook" },
+  {
+    $set: {
+      appId: Meteor.settings.oAuth.facebook.appId,
+      secret: Meteor.settings.oAuth.facebook.secret,
+      loginStyle: "redirect",
+    },
+  }
+);
 
 Accounts.onCreateUser(({ profile }, user) => {
   const profileWithPicture = profile;
