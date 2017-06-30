e5fb077ebe82166a9010ff4f3289deb1e25d52b8
stevenjmarsh <sjm.atl@gmail.com>
2017-06-30 17:49:52 -0400

    testing inappbrowser cordova plugin (to resolve fb login redirect)

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -8,7 +8,7 @@ ServiceConfiguration.configurations.upsert(
     $set: {
       appId: Meteor.settings.oAuth.facebook.appId,
       secret: Meteor.settings.oAuth.facebook.secret,
-      loginStyle: "popup",
+      loginStyle: "redirect",
     },
   }
 );
